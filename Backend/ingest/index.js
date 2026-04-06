61b87cfa592665cbb9dd84be90536ad980ea41f1
Pratyush <letscode269@gmail.com>
2026-04-06 21:34:01 +0530

    Project Done

---
+++ Backend/ingest/index.js
@@ -0,0 +1,92 @@
+import { Inngest } from "inngest";
+import prisma from "../Configs/prisma.js";
+
+export const inngest = new Inngest({ id: "profile-marketplace" });
+
+//function to save tthe userdata to our database
+const syncUserCreation = inngest.createFunction(
+  { id: "sync-user-from-clerk" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    const { data } = event;
+
+    //checking if user already exists in the database.
+
+    const user = await prisma.user.findFirst({
+      where: { id: data.id },
+    });
+
+    if (user) {
+      await prisma.user.update({
+        where: { id: data.id },
+        data: {
+          email: data?.email_addresses[0]?.email_address,
+          name: data?.first_name + " " + data?.last_name,
+          image: data?.image_url,
+        },
+      });
+      return;
+    }
+    await prisma.user.create({
+      data: {
+        id: data.id,
+        email: data?.email_addresses[0]?.email_address,
+        name: data?.first_name + " " + data?.last_name,
+        image: data?.image_url,
+      },
+    });
+  },
+);
+
+//function to delete user from database.
+const syncUserDeletion = inngest.createFunction(
+  { id: "delete-user-from-clerk" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    const { data } = event;
+    const listings = await prisma.listing.findMany({
+      where: { ownerId: data.id },
+    });
+    const chats = await prisma.chat.findMany({
+      where: {
+        OR: [{ ownerUserId: data.id }, { chatUserId: data.id }],
+      },
+    });
+    const transactions = await prisma.transaction.findMany({
+      where: { userId: data.id },
+    });
+
+    if (
+      listings.length === 0 &&
+      chats.length === 0 &&
+      transactions.length === 0
+    ) {
+      await prisma.user.delete({ where: { id: data.id } });
+    } else {
+      await prisma.listing.updateMany({
+        where: { ownerId: data.id },
+        data: { status: "inactive" },
+      });
+    }
+  },
+);
+
+//fuction to update user data in database.
+const syncUserUpdation = inngest.createFunction(
+  { id: "update-user-from-clerk" },
+  { event: "clerk/user.updated" },
+  async ({ event }) => {
+    const { data } = event;
+
+    await prisma.user.update({
+      where: { id: data.id },
+      data: {
+        email: data?.email_addresses[0]?.email_address,
+        name: data?.first_name + " " + data?.last_name,
+        image: data?.image_url,
+      },
+    });
+  },
+);
+
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
